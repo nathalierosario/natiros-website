1cc71c7c4aa00f18869dd722a21af32f697ba896
nathalierosario <91161177+nathalierosario@users.noreply.github.com>
2024-01-24 13:51:09 -0500

    lifting state up

--- src/components/GoldenEye/Pattern.tsx
+++ src/components/GoldenEye/Pattern.tsx
@@ -21,7 +21,35 @@ export default function Pattern() {
     O: Array(5).fill(false),
   };
 
+  const dontCall: string[] = [];
+
   const [pattern, setPattern] = useState<BingoPattern>(initialPattern);
+  const [dontCallLetters, setDontCallLetters] = useState<string[]>(dontCall);
+
+  const clickPattern = (letter: keyof BingoPattern, index: number) => {
+    setPattern((prevPattern) => {
+      const updatedCells = [...prevPattern[letter]];
+      updatedCells[index] = !updatedCells[index];
+
+      const updatedPattern = {
+        ...prevPattern,
+        [letter]: updatedCells,
+      };
+
+      updateDontCall(updatedPattern);
+
+      return updatedPattern;
+    });
+  };
+
+  const updateDontCall = (updatedPattern: BingoPattern) => {
+    const bingoLetters = Object.keys(updatedPattern) as (keyof BingoPattern)[];
+    const updatedDontCall = bingoLetters.filter((l) =>
+      updatedPattern[l].every((val) => !val)
+    );
+
+    setDontCallLetters(updatedDontCall.length === 5 ? [] : updatedDontCall);
+  };
 
   return (
     <Container className="text-center pattern-board d-inline-flex w-auto">
@@ -29,10 +57,17 @@ export default function Pattern() {
         {Object.keys(pattern).map((key) => (
           <Col key={key} className="flex-grow-0 pattern-bingo-letter">
             {key}
-            {pattern[key as keyof BingoPattern].map((value, index) => (
-              <Row>
+            {pattern[key as keyof BingoPattern].map((isClicked, index) => (
+              <Row key={index}>
                 <Col className="flex-grow-0">
-                  <Button variant="outline-dark" key={index} className="pattern-cell-btn">
+                  <Button
+                    onClick={() =>
+                      clickPattern(key as keyof BingoPattern, index)
+                    }
+                    variant={isClicked ? "danger" : "outline-danger"}
+                    key={key + index}
+                    className="pattern-cell-btn"
+                  >
                     {key === "N" && index === 2 ? "FREE SPACE" : key + index}
                   </Button>
                 </Col>
